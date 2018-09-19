c6e980d02e0c9f1545a78f850b290f3fb92dde8b
bjorn <bertus.men@gmail.com>
2018-09-19 10:06:44 +0200

    push

--- Server.js
+++ Server.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 8000;
+	var express = require('express');
+	var app = express();
+	var http = require('http').Server(app);
+	var io = require('socket.io')(http);
+	var port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + '/public'));
+	app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
-	var lastPosition = { x: 0, y: 0 };
+	io.on('connection', function(socket){
+		var lastPosition = { x: 0, y: 0 };
 
-	socket.emit('update_position', lastPosition);
+		socket.emit('update_position', lastPosition);
 
-	socket.on('receive_position', function (data) {
-		lastPosition = data;
-		socket.broadcast.emit('update_position', data);
-		console.log(data);
+		socket.on('receive_position', function (data) {
+			lastPosition = data;
+			socket.broadcast.emit('update_position', data);
+			console.log(data);
+		});
 	});
-});
 
-http.listen(port, function(){
-	console.log('listening on [ip]:' + port);
-});
+	http.listen(port, function(){
+		console.log('listening on [ip]:' + port);
+	});
 
-//app.get('/', function(req, res){
-//	res.sendFile(__dirname + '/public/index.html');       //not needed since document is called index
-//});
+	//app.get('/', function(req, res){
+	//	res.sendFile(__dirname + '/public/index.html');       //not needed since document is called index
+	//});
