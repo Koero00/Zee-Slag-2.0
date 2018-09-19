79cefba5583b74e16390cf43e762273c6f934872
bjorn <bertus.men@gmail.com>
2018-09-19 09:58:05 +0200

    movement

--- Server.js
+++ Server.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/public/index.html');
+io.on('connection', function(socket){
+	var lastPosition = { x: 0, y: 0 };
+
+	socket.emit('update_position', lastPosition);
+
+	socket.on('receive_position', function (data) {
+		lastPosition = data;
+		socket.broadcast.emit('update_position', data);
+		console.log(data);
+	});
 });
 
-server.listen(port, function(){
-	console.log("Server running on port 8000");
+http.listen(port, function(){
+	console.log('listening on [ip]:' + port);
 });
+
+//app.get('/', function(req, res){
+//	res.sendFile(__dirname + '/public/index.html');       //not needed since document is called index
+//});
